4a5d8c40214e7052572279cc8b09bf765fb1aac4
Iker González Camacho <74884509+Ikergonzalez4@users.noreply.github.com>
2024-01-05 22:00:12 +0100

    Update Home.js
    key && font load (error control)

--- src/Home.js
+++ src/Home.js
@@ -1,53 +1,62 @@
 import React from "react";
-import { TouchableOpacity, Text, View, SafeAreaView, ScrollView } from 'react-native';
-import { useFonts } from 'expo-font';
+import {
+  TouchableOpacity,
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
+import { useFonts } from "expo-font";
 import styles from "../styles/Styles";
-import CarData from "../json/car-list .json";
-import { AntDesign } from '@expo/vector-icons';
+import CarData from "../json/car-list.json";
+import { AntDesign } from "@expo/vector-icons";
 
 function Home() {
+  const [fontsLoaded, error] = useFonts({
+    "AlfaSlabOne-Regular": require("../assets/fonts/AlfaSlabOne-Regular.ttf"),
+    "Ubuntu-Bold": require("../assets/fonts/Ubuntu-Bold.ttf"),
+  });
 
-    const [fontsLoaded] = useFonts({
-        'AlfaSlabOne-Regular': require('../assets/fonts/AlfaSlabOne-Regular.ttf'),
-        'Ubuntu-Bold': require('../assets/fonts/Ubuntu-Bold.ttf'),
-    });
+  if (error) {
+    console.error("Error cargando fuentes:", error);
+    return <Text>Error cargando fuentes</Text>;
+  }
 
-    if (!fontsLoaded) {
-        return null;
-    }
+  if (!fontsLoaded) {
+    return null;
+  }
 
-    return (
-        <View style={styles.View1}>
-            <SafeAreaView style={styles.SafeAreaView}>
-                <Text style={styles.Title}>Cochedex</Text>
-                <View style={{ height: 20 }} />
-                <TouchableOpacity style={styles.SearchBar}>
-                    <Text style={styles.SomeTextDark}>Search</Text>
-                </TouchableOpacity>
-                <View style={{ height: 20 }} />
-                <ScrollView>
-                    {CarData.map((brandItem) => (
-                        <View key={brandItem.brand}>
-                            <Text style={styles.BrandText}>{brandItem.brand}</Text>
-                            <View>
-                                {brandItem.models.map((model) => (
-                                    <View key={model}>
-                                        <Text style={styles.ModelText}> {brandItem.brand} · {model}</Text>
-                                        <TouchableOpacity style={styles.InsertFoto}>
-                                            <AntDesign name="plussquareo" size={48} color="black" />
-                                        </TouchableOpacity>
-
-                                    </View>
-
-                                ))}
-                            </View>
-                        </View>
-                    ))}
-                </ScrollView>
-
-            </SafeAreaView>
-        </View>
-    );
+  return (
+    <View style={styles.View1}>
+      <SafeAreaView style={styles.SafeAreaView}>
+        <Text style={styles.Title}>Cochedex</Text>
+        <View style={styles.spacer20} />
+        <TouchableOpacity style={styles.SearchBar}>
+          <Text style={styles.SomeTextDark}>Search</Text>
+        </TouchableOpacity>
+        <View style={styles.spacer20} />
+        <ScrollView>
+          {CarData.map((brandItem) => (
+            <View key={brandItem.brand}>
+              <Text style={styles.BrandText}>{brandItem.brand}</Text>
+              <View>
+                {brandItem.models.map((model) => (
+                  <View key={model}>
+                    <Text style={styles.ModelText}>
+                      {brandItem.brand} · {model}
+                    </Text>
+                    <TouchableOpacity style={styles.InsertFoto}>
+                      <AntDesign name="plussquareo" size={48} color="black" />
+                    </TouchableOpacity>
+                  </View>
+                ))}
+              </View>
+            </View>
+          ))}
+        </ScrollView>
+      </SafeAreaView>
+    </View>
+  );
 }
 
 export default Home;
